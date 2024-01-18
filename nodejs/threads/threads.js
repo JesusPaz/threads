284e54cba99e7b93e0c85719a2863968add0d69c
Jesus Hernando Paz Bolaños <jesus@nodesource.com>
2024-01-17 21:49:16 -0500

    update nodejs threads

--- nodejs/threads/threads.js
+++ nodejs/threads/threads.js
@@ -1,11 +1,35 @@
-const { Worker } = require('worker_threads');
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
-function main() {
-    const numThreads = 1;
-
-    for (let i = 0; i < numThreads; i++) {
-        new Worker('./worker.js');
-    }
+function startWorker(path, cb) {
+    const worker = new Worker(path);
+    worker.on('message', (msg) => cb(null, msg));
+    worker.on('error', cb);
+    worker.on('exit', (code) => {
+        if (code !== 0)
+            cb(new Error(`Worker stopped with exit code ${code}`));
+    });
+    return worker;
 }
 
-main();
+if (isMainThread) {
+    const numThreads = 4;
+
+    for (let i = 0; i < numThreads; i++) {
+        startWorker(__filename, (err, result) => {
+            if (err) return console.error(err);
+            console.log(result);
+        });
+    }
+
+} else {
+    async function performSimpleOperation() {
+        while (true) {
+            let _ = 42 * 42;
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    }
+
+    performSimpleOperation().then(() => {
+        parentPort.postMessage('done');
+    });
+}
